0e23bd29d2ca7042c07df237520996dcb3584d49
Lieselotte <52315535+she3py@users.noreply.github.com>
2025-02-22 23:10:03 +0100

    feat(hive): connect to websocket

--- hive/src/main.rs
+++ hive/src/main.rs
@@ -1,14 +1,19 @@
 #![cfg(target_arch = "wasm32")]
 
+#[macro_use]
+extern crate log;
+
 use eframe::{WebLogger, WebOptions, WebRunner};
-use hive::Hive;
 use log::LevelFilter;
+use web_sys::WebSocket;
+use eframe::wasm_bindgen::JsCast as _;
+use crate::app::Hive;
+
+mod app;
 
 fn main() {
-    use eframe::wasm_bindgen::JsCast as _;
-    
-    // Redirect `log` message to `console.log` and friends:
     WebLogger::init(LevelFilter::Debug).ok();
+    info!("Démarrage de l'application...");
     
     let web_options = WebOptions::default();
     
@@ -18,25 +23,30 @@ fn main() {
             .document()
             .expect("No document");
         
+        // Connexion au websocket
+        let thighs = WebSocket::new("ws://localhost:8000").unwrap();
+        
         let canvas = document
             .get_element_by_id("the_canvas_id")
             .expect("Failed to find the_canvas_id")
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .expect("the_canvas_id was not a HtmlCanvasElement");
         
+        // Start the app in a web runner
         let start_result = WebRunner::new()
             .start(
                 canvas,
                 web_options,
-                Box::new(|rcx| Ok(Box::new(Hive::new(rcx)))),
+                Box::new(|rcx| Ok(Box::new(Hive::new(rcx, thighs)))),
             )
             .await;
         
-        // Remove the loading text and spinner:
+        // Remove the loading text and spinner
         if let Some(loading_text) = document.get_element_by_id("loading_text") {
             match start_result {
                 Ok(_) => {
                     loading_text.remove();
+                    info!("Application démarrée.")
                 }
                 Err(e) => {
                     loading_text.set_inner_html(
