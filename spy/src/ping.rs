8d1975a84061667abaf950997c3e41808f3cadca
Lieselotte <52315535+she3py@users.noreply.github.com>
2025-03-05 21:29:46 +0100

    feat: add spy service

--- spy/src/ping.rs
+++ spy/src/ping.rs
@@ -1,51 +1,50 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
-use std::process::exit;
+use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::time::Duration;
 use tungstenite::{Bytes, Message};
 
-fn main() {
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 3000);
+pub fn ping(ip: IpAddr) -> Result<(), ()> {
+    let addr = SocketAddr::new(ip, 3000);
     let stream = match TcpStream::connect(addr) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("connect: {e}");
-            exit(1);
+            return Err(());
         }
     };
     if let Err(e) = stream.set_read_timeout(Some(Duration::from_secs(1))) {
         eprintln!("setsockopt: {e}");
-        exit(1);
+        return Err(());
     }
     
     let (mut socket, response) = match tungstenite::client("ws://localhost:3000/socket", stream) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("handshake: {e}");
-            exit(1);
+            return Err(());
         }
     };
     
-    println!("Connected to the server, version {:?}", response.version());
-    println!("Status: {}", response.status());
-    println!("Headers:");
-    for (header, value) in response.headers() {
-        println!("* {header} = {value}", value = String::from_utf8_lossy(value.as_bytes()));
+    if response.status() != 101 /* Switching Protocol */ {
+        eprintln!("status: {}", response.status());
+        return Err(());
     }
     
     if let Err(e) = socket.send(Message::Ping(Bytes::from_static(b"xoxo"))) {
         eprintln!("send: {e}");
-        exit(1);
+        return Err(());
     }
     
     match socket.read() {
-        Ok(m) => {
-            println!("{m:?}");
+        Ok(_) => {
+            // we don't care about the message we received, the worker's up so all's good
+            println!("OK");
         }
         Err(e) => {
             eprintln!("recv: {e}");
-            exit(1);
+            return Err(());
         }
     }
     
     _ = socket.close(None);
+    Ok(())
 }
---
+++ spy/src/stack.rs
@@ -0,0 +1,27 @@
+use serde::Deserialize;
+use std::ffi::OsStr;
+
+#[derive(Debug, Deserialize)]
+pub struct Task {
+    #[serde(rename = "Name")]
+    pub name: String,
+    
+    #[serde(rename = "ID")]
+    pub id: String,
+}
+
+pub fn ps(stack: impl AsRef<OsStr>) -> Vec<Task> {
+    let stdout = super::docker(["stack", "ps"], stack);
+    
+    stdout.split(|b| *b == b'\n')
+        .filter_map(|line| Some(line.trim_ascii()).filter(|line| !line.is_empty()))
+        .filter_map(|line| match serde_json::from_slice::<Task>(line) {
+            Ok(task) => Some(task),
+            Err(e) => {
+                eprintln!("error: failed to parse the following as a task: {e}");
+                eprintln!("{}", String::from_utf8_lossy(line));
+                None
+            }
+        })
+        .collect()
+}
